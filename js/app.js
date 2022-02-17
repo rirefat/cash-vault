cce6e6c0cfbde32367bc5c5378ee312d0ed5a5f8
rirefat <rirefat.official@gmail.com>
2022-02-17 07:00:16 +0600

    Added Savings Functionality

--- js/app.js
+++ js/app.js
@@ -8,7 +8,21 @@ document.getElementById('calculate-cost').addEventListener('click',function(){
     document.getElementById('total-expense').innerText = totalCost.toFixed(2);
 
     let totalIncome = parseFloat(document.getElementById('income').value);
-    console.log(totalIncome);
+    // console.log(totalIncome);
     let balance = totalIncome - totalCost;
     document.getElementById('balance').innerText = balance.toFixed(2);
+})
+
+// Savings Calculation
+document.getElementById('save-btn').addEventListener('click', function(){
+    let totalIncome = parseFloat(document.getElementById('income').value);
+    // console.log(totalIncome)
+    let savingsRate = parseFloat(document.getElementById('savings-rate').value);
+    // console.log(savingsRate)
+    let savingsAmount = totalIncome * (savingsRate/100);
+    document.getElementById('savings-amount').innerText = savingsAmount.toFixed(2);
+    let previousBalance = parseFloat(document.getElementById('balance').innerText);
+    let remainingBalance = previousBalance - savingsAmount;
+    // console.log(remainingBalance)
+    document.getElementById('remaining-balance').innerText = remainingBalance.toFixed(2);
 })
